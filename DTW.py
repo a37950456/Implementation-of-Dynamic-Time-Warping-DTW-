96a1fca47e91e23b87dedf843278bfa17fccd9fa
ChiaTien <a37950456@yahoo.com.tw>
2018-06-29 10:53:32 +0800

    Update DTW.py

--- DTW.py
+++ DTW.py
@@ -10,15 +10,15 @@ import numpy as np
 def DTW(obser,obser_):
     n = len(obser)
     m = len(obser_)
-    DP = np.zeros((n,m))   #n*m的矩陣
+    DP = np.zeros((n,m))   #n*m 
     Dis = np.zeros((n,m))
     
-    #生成原始距离矩阵
+    #Original Distances array
     for i in range(0,n,1):
         for j in range(0,m,1):
                 Dis[i,j] = abs(obser_[j-1]-obser[i-1])
         
-    #最短距離    
+    #shortest path    
     for i in range(0,n,1):
             DP[i,0] += Dis[i,0]
     for i in range(0,n,1):
